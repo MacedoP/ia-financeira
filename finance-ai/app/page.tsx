411ce0396c57d784719c2717b724bbd72d9ddb01
Macedo Francisco <macedolb53@gmail.com>
2024-11-06 19:33:38 +0100

    chore: add shadcn setup

--- finance-ai/app/page.tsx
+++ finance-ai/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <div>
       <h1 className="mt-6 text-center text-3xl text-red-500">
         Welcome to my home page
       </h1>
+
+      <Button variant="outline">Clicar</Button>
     </div>
   );
 };
