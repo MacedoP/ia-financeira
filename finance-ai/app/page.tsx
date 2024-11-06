304667574bce8156699177a6cb2834776b51a7d6
Macedo Francisco <macedolb53@gmail.com>
2024-11-06 19:09:21 +0100

    chore: add prettier and eslint plugin

--- finance-ai/app/page.tsx
+++ finance-ai/app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
-  return ( 
+  return (
     <div>
-      <h1>Welcome to my home page</h1>
+      <h1 className="mt-6 text-center text-3xl text-red-500">
+        Welcome to my home page
+      </h1>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
