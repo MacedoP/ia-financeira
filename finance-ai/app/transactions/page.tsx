5ba723a00b542e6969621efbe8a8a65eb341cb85
Macedo Francisco <macedolb53@gmail.com>
2024-11-07 01:10:09 +0100

    feat: add transaction page setup

--- finance-ai/app/transactions/page.tsx
+++ finance-ai/app/transactions/page.tsx
@@ -1,6 +1,28 @@
-const TransactionsPage = () => {
+import { ArrowDownUp } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+
+    //chamando o nosso banco de dados
+    const transactions = await db.transaction.findMany({
+
+        
+    })
     return ( 
-        <div>
+        <div className="p-6 space-y-6 ">
+            {/**** Titulo e  botão ****/}
+           <div className="flex justify-between w-full items-center">
+            <h1 className="font-bold">Transactions</h1>
+            <Button className="rounded-full">
+                Add Transactions
+                <ArrowDownUp className="ml-1"/>
+            </Button>
+           </div>
+
+           <DataTable columns={transactionColumns}  data={transactions} />
             
         </div>
      );
